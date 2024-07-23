f02f7f63c8c5a67fadfb68f535f04ba2eea82e69
mohitAvijo <it@avijo.in>
2024-07-23 12:42:58 +0530

    Update BaseUrl.js

--- src/Routes/BaseUrl.js
+++ src/Routes/BaseUrl.js
@@ -1,3 +1,3 @@
-export const BaseUrl = "https://avijobackend-production-2eae.up.railway.app/";
+export const BaseUrl = "https://avijobackend-production-2eae.up.railway.app";
 // export const BaseUrl = "https://avijo-1.onrender.com";
 export const CarrersBaseUrl = "https://avijocareer-production.up.railway.app";
